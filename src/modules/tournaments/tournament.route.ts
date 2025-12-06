165e68bf6f72b0fe0988a90d5b4a3f15b997aea7
Ephrem Mesfin <efa.traww21@gmail.com>
2025-12-06 16:51:49 +0300

    tournament serivecs implementted

--- src/modules/tournaments/tournament.route.ts
+++ src/modules/tournaments/tournament.route.ts
@@ -7,4 +7,16 @@ router.get("/", TournamentController.getTournaments);
 
 router.get("/:id", TournamentController.getTournament);
 
+router.get("/:tournamentId/teams", TournamentController.getTournamentTeams);
+
+router.get(
+  "/:tournamentId/fixtures",
+  TournamentController.getTournamentFixtures
+);
+
+router.get(
+  "/:tournamentId/standings",
+  TournamentController.getTournamentStandings
+);
+
 export default router;
