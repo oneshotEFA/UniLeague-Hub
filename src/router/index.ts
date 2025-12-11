bb71259a824eeb14427190e3a7f5a92a92d80765
kirubel4 <kirubelayele2127@gmail.com>
2025-12-11 22:03:01 +0300

    player route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import authRouter from "../modules/auth/auth.route";
 import tournamentRoute from "../modules/tournaments/tournament.route";
 import aiRoute from "../modules/_AI/aiRoute";
+import playerRouter from "../modules/players/player.route";
 
 const apiRouter = Router();
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/tournaments", tournamentRoute);
 apiRouter.use("/ai", aiRoute); // Placeholder for user routes
+apiRouter.use("/player", playerRouter)
 export default apiRouter;
