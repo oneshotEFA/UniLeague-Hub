da7ab13e43b82f8c606babb5db30c612f0e628ad
kirubel4 <kirubelayele2127@gmail.com>
2026-01-09 22:44:31 +0300

    controller and route

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -7,5 +7,6 @@ router.post("/signup", AuthController.signup);
 router.post("/login", AuthController.login);
 router.post("/user/update", AuthController.updateUser);
 router.get("/user/me/:id", AuthController.getMe);
+router.post("/changePassword", AuthController.changePassword)
 
 export default router;
