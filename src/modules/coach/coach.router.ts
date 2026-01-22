3e49122ec6ca3c71efb1eb7bfc985cd6c1c7dfe5
Ephrem Mesfin <efa.traww21@gmail.com>
2026-01-22 14:40:49 +0300

    coach router fixed

--- src/modules/coach/coach.router.ts
+++ src/modules/coach/coach.router.ts
@@ -3,20 +3,7 @@ import { CoachController } from "./coach.controller";
 import { reqAuth } from "../../middlewares/reqAuth";
 import { requireAdmin } from "../../middlewares/reqPermission";
 const route = Router();
-route.post(
-  "/request/line-up",
-  reqAuth,
-
-  CoachController.RequestLineUp,
-);
-route.get(
-  "/match/line-up/:id/:teamId",
-  CoachController.getLineUpOfMatch,
-);
-route.get(
-  "/line-up/record/:teamId",
-  reqAuth,
-
-  CoachController.getLineupHistory,
-);
+route.post("/request/line-up", reqAuth, CoachController.RequestLineUp);
+route.get("/match/line-up/:id/:teamId", CoachController.getLineUpOfMatch);
+route.get("/line-up/record/:teamId", reqAuth, CoachController.getLineupHistory);
 export default route;
